8803638830e33560764251dd2f0836c5b4dc5c24
austrisD <austris.daugulis@gmail.com>
2021-07-12 14:00:16 +0300

    paginator finished

--- src/app/components/_SharedElements/page-control/page-control.component.ts
+++ src/app/components/_SharedElements/page-control/page-control.component.ts
@@ -5,42 +5,42 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
   templateUrl: './page-control.component.html',
 })
 export class PageControlComponent implements OnInit {
-  @Input() pageCount: any = 10;
-  @Output() newActivePage = new EventEmitter();
+  @Input() pageCount!: any;
 
   @Input() activePage!: number;
-  // pageNR: number = 1;
+  @Output() newActivePage = new EventEmitter<number>();
+
+  nr!: number;
   constructor() {}
 
   previousPage() {
     const isPageFalse = this.activePage <= 0; //check if page is not less then 0/
     if (!isPageFalse) this.activePage = this.activePage - 1;
-    // this.newActivePage.emit(this.activePage);
+    this.newActivePage.emit(this.activePage);
     console.log(this.activePage);
   }
 
   nextPage() {
-    const isPageFalse = this.activePage >= this.pageCount; //check if active page is no more then awaitable.
+    const isPageFalse = this.activePage >= this.pageCount.length; //check if active page is no more then awaitable.
     if (!isPageFalse) this.activePage = this.activePage + 1;
-    // this.newActivePage.emit(this.activePage);
+    this.newActivePage.emit(this.activePage);
     console.log(this.activePage);
   }
 
-  setPage(pageNR) {
-    this.newActivePage.emit(pageNR + 1);
+  setPage(nr: number) {
+    this.activePage = nr + 1;
+    this.newActivePage.emit(this.activePage);
+    console.log(this.activePage);
   }
 
   ngOnInit(): void {
-    if (this.pageCount === Number) {
+    if (typeof this.pageCount == 'number') {
       this.pageCount = Array(this.pageCount)
         .fill(0)
         .map((x, i) => i);
-    } else if (this.pageCount == Object) {
+    } else if (typeof this.pageCount == 'object') {
       this.pageCount = Object.keys(this.pageCount).length;
     }
-
-    console.log(this.pageCount);
-
-    //generate a array for loop
+    // generate a array for loop
   }
 }
--- src/app/components/favorite-games/favorite-games.component.ts
+++ src/app/components/favorite-games/favorite-games.component.ts
@@ -7,8 +7,8 @@ import { ManageFavoriteService } from '../../services/manage-favorite.service';
 })
 export class FavoriteGamesComponent implements OnInit {
   favorites: any[] = [];
-  activePage: number = this.manageFavoriteService.activePage;
-  pageCount!: number;
+  activePage = this.manageFavoriteService.activePage;
+  pageCount: number = 10;
 
   constructor(private manageFavoriteService: ManageFavoriteService) {}
 
@@ -16,7 +16,7 @@ export class FavoriteGamesComponent implements OnInit {
     this.manageFavoriteService
       .getFavorites()
       .subscribe((favorites) => (this.favorites = favorites));
-    this.pageCount = Object.keys(this.favorites).length;
+    // this.pageCount = Object.keys(this.favorites).length;
   }
 
   test(pageCount) {
@@ -29,8 +29,8 @@ export class FavoriteGamesComponent implements OnInit {
     console.log('this record is deleted');
   }
 
-  reloadData() {
-    this.favorites = this.favorites;
-    console.log(this.activePage);
-  }
+  // reloadData() {
+  //   this.favorites = this.favorites;
+  //   console.log(this.activePage);
+  // }
 }
